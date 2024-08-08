7056451682274a131a5a3e64d92a9c7ea2e53d13
GyeongjunHa <32204781@dankook.ac.kr>
2024-08-08 23:38:33 +0900

    feat: Domain design(No-refactoring)

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;  //주문 회원
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery; //배송 정보
+
+    private LocalDateTime orderDate; //주문 시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderState status;
+
+}
